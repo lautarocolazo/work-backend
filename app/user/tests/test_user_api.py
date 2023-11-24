4270f77e235671b88e3e85a410bf096cf9967bcb
Lautaro Colazo <lautarocolazo1@gmail.com>
2023-11-24 13:02:05 -0700

    fixing linting

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -14,7 +14,7 @@ CREATE_USER_URL = reverse('user:create')
 
 def create_user(**params):
     """Create and return a new user."""
-    return get_user_model().objects.create-user(**params)
+    return get_user_model().objects.create_user(**params)
 
 
 class PublicUserApiTests(TestCase):
